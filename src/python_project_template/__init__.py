2ec09c8806d02c050cb8fa7820af6934766253de
Robert <golgafrincham@gmail.com>
2024-09-13 15:17:53 +0200

    Add mkdocs (#24)
    * add mkdocs
    
    * add init for package and script
    
    * add extentions and remove unused configuration
    
    * add comment about deploy to github pages
    
    * Update docs/index.md
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    * styling
    
    * added examples for documentation
    
    * add nested navbar example
    
    ---------
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

---
+++ src/python_project_template/__init__.py
@@ -0,0 +1,14 @@
+# --8<-- [start:hello]
+def hello(name: str) -> str:
+    """Simple Hello-function.
+
+    Args:
+        name (str): A name to say hello to.
+
+    Returns:
+        str: A hello string with the name contained.
+    """
+    return f"Hello, {name}!"
+
+
+# --8<-- [end:hello]
